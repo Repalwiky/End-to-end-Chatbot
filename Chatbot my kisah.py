837c751468e72531cd2f1d88b6998835e5d497d6
Repalwiky <ngabthomi@gmail.com>
2025-03-13 19:54:11 +0700

    Update Chatbot my kisah.py

--- Chatbot my kisah.py
+++ Chatbot my kisah.py
@@ -4,6 +4,7 @@ import nltk
 import ssl
 import streamlit as st
 import random
+import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
